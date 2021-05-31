841eef8c71d1da7c38ee85540a740bc7c28c48a5
이효진 <billion109@naver.com>
2021-05-31 15:03:09 +0900

    solved

--- 백준/1562 계단 수.py
+++ 백준/1562 계단 수.py
@@ -1,63 +1,20 @@
-from collections import defaultdict
-
-
-def fun(x):
-    temp = []
-    a = x[0]
-    b = x[1]
-
-    if a == '9':
-        if b == '0':
-            temp.append('80')
-            temp.append('91')
-        elif b == '9':
-            temp.append('89')
-            temp.append('98')
-        else:
-            temp.append('8' + b)
-            temp.append('9' + str(int(b) + 1))
-            temp.append('9' + str(int(b) - 1))
-    else:
-        if b == '0':
-            temp.append(str(int(a) + 1) + '0')
-            if a != '1':
-                temp.append(str(int(a) - 1) + '0')
-            temp.append(a + '1')
-        elif b == '9':
-            temp.append(str(int(a) + 1) + '9')
-            if a != '1':
-                temp.append(str(int(a) - 1) + '9')
-            temp.append(a + '8')
-        else:
-            temp.append(str(int(a) + 1) + b)
-            if a != '1':
-                temp.append(str(int(a) - 1) + b)
-            temp.append(a + str(int(b) + 1))
-            temp.append(a + str(int(b) - 1))
-
-    return temp
-
-
 n = int(input())
-arr = [{'90': 1}]
-ans = [1]
+dp = [[0 for _ in range(1 << 10)] for _ in range(10)]
+mod = 1000000000
 
-for i in range(n - 10):
-    prev = arr[i]
-    temp = defaultdict(int)
-    temp_val = 0
-    for k, v in prev.items():
-        temp_list = fun(k)
-        for j in temp_list:
-            temp[j] += v
-            temp_val += v
-    arr.append(temp)
-    ans.append(temp_val)
+for i in range(1, 10):
+    dp[i][1 << i] = 1
 
-print(arr)
-print(ans)
-print(sum(ans))
+for i in range(1, n):
+    new_dp = [[0 for _ in range(1 << 10)] for _ in range(10)]
+    for e in range(10):
+        for bm in range(1 << 10):
+            if e < 9:
+                new_dp[e][bm | (1 << e)] += dp[e + 1][bm]
+                new_dp[e][bm | (1 << e)] %= mod
+            if e > 0:
+                new_dp[e][bm | (1 << e)] += dp[e - 1][bm]
+                new_dp[e][bm | (1 << e)] %= mod
+    dp = new_dp
 
-aawe = ['90', '99', '88', '11', '10','99','19','18']
-for i in aawe:
-    print(i + ':' , fun(i))
+print(sum([dp[i][1023] for i in range(10)]) % mod)
