0b25f05561dca58bddcdb73792c942d1836bbd0e
이효진 <billion109@naver.com>
2021-06-19 23:09:07 +0900

    solved

---
+++ 백준/11282 한글.py
@@ -0,0 +1,2 @@
+# print(chr(44031+int(input())))
+print(ord(input().rstrip())-44031)
---
+++ 백준/16170 오늘의 날짜는.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+print(datetime.utcnow().year)
+print(datetime.utcnow().month)
+print(datetime.utcnow().day)
